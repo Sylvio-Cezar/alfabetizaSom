6f8b8fe8fb7d888051591f1999d8253a02741e14
jeanarthur <jean.arthursa@gmail.com>
2023-08-18 20:57:24 -0300

    adiciona modulo

---
+++ src/app/tab4/tab4.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { Tab4PageRoutingModule } from './tab4-routing.module';
+
+import { Tab4Page } from './tab4.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ExploreContainerComponentModule,
+    Tab4PageRoutingModule
+  ],
+  declarations: [Tab4Page]
+})
+export class Tab4PageModule {}
