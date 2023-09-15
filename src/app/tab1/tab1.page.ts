db8facb6f6a611942775602e6b529208f5f3ec57
Sylvio-Cezar <sylvio.015@gmail.com>
2023-09-15 09:17:13 -0300

    correção audio

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,43 +1,27 @@
 import { Component } from '@angular/core';
-import { Media, MediaObject } from '@ionic-native/media/ngx';
 import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  providers: [Media]
 })
 export class Tab1Page {
 
-  public letra: String = "A";
+  public letra: string = "A";
   public isPhoneme: boolean = true;
-  public imagemDetalheLetra: String = "";
+  public imagemDetalheLetra: string = "";
+  public snd = new Audio("../../assets/sounds/sfx-pop.mp3");
 
-  private mediaObject!: MediaObject;
 
-  constructor(
-      private media: Media,
-      private platform: Platform
-    ) {}
+  constructor() {}
 
   ngOnInit() {
-    // Ensure the platform is ready before initializing media
-    this.platform.ready().then(() => {
-      this.mediaObject = this.media.create('../../assets/sounds/sfx-pop.mp3');
-
-      this.mediaObject.onError.subscribe(error => {
-        console.error('Media initialization error:', error);
-      });
-    });
-
     this.imagemDetalheLetra = "../../assets/images/letras-detalhes/Detalhes-" + this.letra + ".png";
   }
 
   playSound() {
-    if (this.mediaObject) {
-      this.mediaObject.play(); // Play the sound
-    }
+    this.snd.play();
   }
 
   changePhoneme(value: boolean) {
