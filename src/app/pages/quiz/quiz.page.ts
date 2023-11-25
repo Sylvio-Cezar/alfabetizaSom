82477629c535c683ccfd853d791a7f063bbbf148
Ruff1es <gabrielfrufino2018@gmail.com>
2023-11-24 21:17:25 -0300

    Aleatorização das Imagens Quiz

--- src/app/pages/quiz/quiz.page.ts
+++ src/app/pages/quiz/quiz.page.ts
@@ -13,6 +13,7 @@ export class QuizPage implements OnInit {
   public letra: Letra = {} as Letra;
   public imagens: Array<String> = [] as Array<String>;
   public imagensQuiz: Array<any> = [] as any;
+  public totalImages: Array<String> = [] as Array<String>
   public pontuation: number = 0;
   public canShowSlide: boolean = true;
   public canGoNext: boolean = false;
@@ -28,11 +29,35 @@ export class QuizPage implements OnInit {
         this.imagensQuiz = this.letra?.imagens_quiz;
       }
      });
+     this.getLetras();
+
+     this.totalImages.push(this.imagens[8])
+     console.log(this.imagens[8])
+     this.totalImages.push(this.imagens[69])
+     this.totalImages.push(this.imagensQuiz[0])
+
+     console.log(this.totalImages)
   }
 
   getLetras() {
     this.alfabetoService.getLetras().subscribe(data => {
       const letras = data.alfabeto_brasileiro;
+      letras.forEach((elem: any) => {
+        let indexImg: Array<any> =  [];
+        indexImg.push(elem.imagens_quiz);
+        indexImg[0].forEach((elem: any) => {
+          this.imagens.push(elem);
+        }); 
+      });
+      this.imagens = this.imagens.filter(elem => !elem.startsWith(`./assets/images/quiz/images/Quiz-${this.letra.nome}`))
+      const shuffle = (array: String[]) => { 
+        for (let i = array.length - 1; i > 0; i--) { 
+          const j = Math.floor(Math.random() * (i + 1)); 
+          [array[i], array[j]] = [array[j], array[i]]; 
+        } 
+        return array; 
+      }; 
+      this.imagens = shuffle(this.imagens);
     });
   }
 
